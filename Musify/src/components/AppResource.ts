ff6b1aad94ab834abd086e2590e7be439ee8f344
Alexandru Vasiu <alexandru.avasiu@gmail.com>
2019-06-16 04:14:44 +0300

    Change API location to Azure
    Signed-off-by: Alexandru Vasiu <alexandru.avasiu@gmail.com>

--- Musify/src/components/AppResource.ts
+++ Musify/src/components/AppResource.ts
@@ -1,2 +1,2 @@
-export const BASE_URL = "http://192.168.100.5:5000/api/";
+export const BASE_URL = "http://musifyapiweb.azurewebsites.net/api/";
 // export const SOCKET_URL =  "http://" + IP + "/ws";
